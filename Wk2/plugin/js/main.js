6d72820e935f6f0dbd32087f419c8efee5f4c39c
BMCII <brian.m.cobbii@fullsail.edu>
2014-12-05 13:18:00 -0500

    plugin

---
+++ Wk2/plugin/js/main.js
@@ -0,0 +1,21 @@
+/*  
+	Your Project Title
+	Author: You
+*/
+
+(function($){
+
+
+
+    $('.countdown').ClassyCountdown({
+        theme: "black-black",
+        end: $.now() + 10000
+    });
+		
+
+	
+})(jQuery); // end private scope
+
+
+
+
