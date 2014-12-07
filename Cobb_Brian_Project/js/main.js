2e8e3fb50acf0843e2a0769082c0adaca4062145
BMCII <brian.m.cobbii@fullsail.edu>
2014-12-06 20:51:16 -0500

    tool tip

--- Cobb_Brian_Project/js/main.js
+++ Cobb_Brian_Project/js/main.js
@@ -1,17 +1,41 @@
 /*  
 	Your Project Title
-	Author: You
+	Author: Brian Cobb
 */
+//alert("hello");
+$(function() {
 
-(function($){
-	
-	
-	
-	
-		
+//================================================= toolTip  =========================================================//
 
+    $('.masterToolTip').hover(function(){
+
+        //================== hover code ===================//
+
+        var title = $(this).attr('title');                      //declare var title to hold title attribute
+        $(this).data('tipText',title).removeAttr('title');      //save var title as data and remove attribute
+        $('<p class = "toolTip"></p>')                          //create HTML <p> element
+            .text(title)                                        //add var title as text in <p> element
+            .appendTo('body')                                   //add <p> element to body
+            .fadeIn('slow');                                    //fade in <p> element
+
+        //================ off hover code =================//
+
+    },function() {
+        $(this).attr('title', $(this).data('tipText'));         //add attribute title with "tipText data"
+        $('.toolTip').remove();                                  //remove tooTip
+    })
+
+        //=============== toolTip position ================//
+
+        .mousemove(function(e){
+            var mousex = e.pageX + 10;                          //create variable to hold x position
+            var mousey = e.pageY + 5;                           //create variable to hold y position
+            $('.toolTip').css({top:mousey, left:mousex});      //select toolTip ans change css
+        });
+
+//==============================================End toolTip  =========================================================//
 	
-})(jQuery); // end private scope
+}); // end private scope
 
 
 
