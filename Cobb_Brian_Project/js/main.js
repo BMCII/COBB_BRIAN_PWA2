806a5f6e6903824c4088e1a4c46d65fd6700d783
BMCII <brian.m.cobbii@fullsail.edu>
2014-12-08 22:00:11 -0500

    clear fix

--- Cobb_Brian_Project/js/main.js
+++ Cobb_Brian_Project/js/main.js
@@ -7,8 +7,8 @@ $(function() {
 
 //=========================================== Drop down login  =======================================================//
 
-    $(document).ready(function() {
-        $('#login p').click(function(){
+    $(document).ready(function () {
+        $('#login p').click(function () {
             $('#login-form').slideToggle(300);
             $(this).toggleClass('close');
         });
@@ -16,12 +16,12 @@ $(function() {
 
 //================================================= toolTip  =========================================================//
 
-    $('.masterToolTip').hover(function(){
+    $('.masterToolTip').hover(function () {
 
         //================== hover code ===================//
 
         var title = $(this).attr('title');                      //declare var title to hold title attribute
-        $(this).data('tipText',title).removeAttr('title');      //save var title as data and remove attribute
+        $(this).data('tipText', title).removeAttr('title');      //save var title as data and remove attribute
         $('<p class = "toolTip"></p>')                          //create HTML <p> element
             .text(title)                                        //add var title as text in <p> element
             .appendTo('body')                                   //add <p> element to body
@@ -29,38 +29,48 @@ $(function() {
 
         //================ off hover code =================//
 
-    },function() {
+    }, function () {
         $(this).attr('title', $(this).data('tipText'));          //add attribute title with "tipText data"
         $('.toolTip').remove();                                  //remove tooTip
     })
 
         //=============== toolTip position ================//
 
-        .mousemove(function(e){
+        .mousemove(function (e) {
             var mousex = e.pageX + 10;                          //create variable to hold x position
             var mousey = e.pageY + 5;                           //create variable to hold y position
-            $('.toolTip').css({top:mousey, left:mousex});       //select toolTip ans change css
+            $('.toolTip').css({top: mousey, left: mousex});       //select toolTip ans change css
         });
 
 //============================================= End toolTip  =========================================================//
 
 //================================================= modal ============================================================//
 
-    $('.modalClick').on('click', function(event){
-        event.preventDefault();
+    $('.modalClick').on('click', function (event) {               //select modalClick and run function on click
+        event.preventDefault();                                 //
         $('#overlay')
             .fadeIn()
             .find('#modal')
             .fadeIn();
     });
 
-    $('.close').on('click', function(event){
+    $('.close').on('click', function (event) {
         event.preventDefault();
         $('#overlay')
             .fadeOut()
             .find('#modal')
             .fadeOut();
     });
+
+    $('.mystatus').mouseover(function () {
+        $(this).fadeTo(100, .3);
+    });
+
+    $('.mystatus').mouseout(function(){
+        $(this).fadeTo(100, 1);
+    });
+
+
 
 //============================================= End modal ============================================================//
 
